a6d44ad2b5555fadfdaac75c3f55926eff6c6bef
Eoin O'Kennedy <Eoin.OKennedy@tudublin.ie>
2025-09-22 16:18:25 +0100

    part 2

--- App.js
+++ App.js
@@ -1,9 +1,17 @@
 import { StyleSheet, Text, View } from 'react-native';
-
+const styles = StyleSheet.create({
+  paragraph: {
+    margin: 36,
+    fontSize: 18,
+    textDecorationLine: 'underline',
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+});
 export default function App() {
   return (
     <View>
-      <Text>Hello, World</Text>
+      <Text style={styles.paragraph}>Hello, World</Text>
     </View>
   );
 };
